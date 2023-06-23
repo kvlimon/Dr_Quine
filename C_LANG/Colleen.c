13e01914e723085cd7b7a311cfcea75bd6f754ff
Ammon <60857611+rooteleven@users.noreply.github.com>
2023-06-24 00:33:34 +0200

    fix: Colleen

--- C_LANG/Colleen.c
+++ C_LANG/Colleen.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#define QUINE "#include <stdio.h>%1$c#define QUINE %2$c %3$s %2$c"
-/* Self replicating */
+#define QUINE "#include <stdio.h>%1$c#define QUINE %2$c%3$s%2$c%1$c/* Self */%1$c%1$cvoid%1$csomething(){}%1$c%1$cint%1$cmain(void)%1$c{%1$c/* Propagation */%1$cprintf(QUINE, 10, 34, QUINE);%1$csomething();%1$creturn 0;%1$c}"
+/* Self */
 
-char*
+void
 something(){}
 
 int
 main(void)
 {
-    /* Propagation */
-    //printf("%s", QUINE, 10);
-    printf(QUINE, 10, 34, QUINE);//, 34);
-    something();
-
-    return 0;
+/* Propagation */
+printf(QUINE, 10, 34, QUINE);
+something();
+return 0;
 }
