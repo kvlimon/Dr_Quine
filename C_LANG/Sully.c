36ea1eddf08f3609a32cdf01deb503cc8aafb74a
Ammon <60857611+rooteleven@users.noreply.github.com>
2023-06-26 19:13:31 +0200

    fix: Sully iteration

--- C_LANG/Sully.c
+++ C_LANG/Sully.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define QUINE "#include <stdio.h>%1$c#include <stdlib.h>%1$c#define QUINE %2$c%3$s%2$c%1$cint X = %4$d;%1$c%1$c\
-int%1$cmain(void)%1$c{%1$cchar str[] = %2$cSully_X.c%2$c;%1$cstr[6] = X + '0';%1$cFILE* fffffff;%1$cfffffff = fopen(str, %2$cw%2$c);%1$cif (fffffff == NULL)%1$c{%1$creturn -1;%1$c}%1$cfprintf(fffffff, QUINE, 10, 34, QUINE, X);%1$c\
-fclose(fffffff);%1$cif (X == 0)%1$c{%1$creturn -1;%1$c}%1$cchar cmdline[100];%1$csprintf(cmdline, %2$cclang -Wall -Wextra -Werror -o %%s Sully ; ./Sully%2$c, str);%1$csystem(cmdline);%1$c--X%1$creturn 0;%1$c}"
+#include <string.h>
+#define QUINE "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <string.h>%1$c#define QUINE %2$c%3$s%2$c%1$c%1$cint X = %4$d;%1$c%1$cint%1$cmain(void)%1$c{%1$cchar next_file[11];%1$cchar next_exec[11];%1$cif ((strchr(__FILE__, '_')) != NULL)%1$c{%1$c--X;%1$c}%1$csprintf(next_file, %2$cSully_%%d.c%2$c, X);%1$csprintf(next_exec, %2$cSully_%%d%2$c, X);%1$c%1$cFILE* fffffff;%1$cfffffff = fopen(next_file, %2$cw%2$c);%1$cif (fffffff == NULL)%1$c{%1$creturn -1;%1$c}%1$cfprintf(fffffff, QUINE, 10, 34, QUINE, X);%1$cfclose(fffffff);%1$cchar cmdline[100];%1$csprintf(cmdline, %2$cclang -Wall -Wextra -Werror %%s -o %%s%2$c, next_file, next_exec);%1$csystem(cmdline);%1$cif (X > 0)%1$c{%1$csprintf(cmdline, %2$c./%%s%2$c, next_exec);%1$csystem(cmdline);%1$c}%1$creturn 0;%1$c}"
+
 int X = 5;
 
 int
 main(void)
 {
-char str[] = "Sully_X.c";
-str[6] = X + '0';
+char next_file[11];
+char next_exec[11];
+if ((strchr(__FILE__, '_')) != NULL)
+{
+--X;
+}
+sprintf(next_file, "Sully_%d.c", X);
+sprintf(next_exec, "Sully_%d", X);
 FILE* fffffff;
-fffffff = fopen(str, "w");
+fffffff = fopen(next_file, "w");
 if (fffffff == NULL)
 {
 return -1;
 }
 fprintf(fffffff, QUINE, 10, 34, QUINE, X);
 fclose(fffffff);
-if (X == 0)
-{
-return -1;
-}
 char cmdline[100];
-sprintf(cmdline, "clang -Wall -Wextra -Werror -o %s Sully ; ./Sully", str);
+sprintf(cmdline, "clang -Wall -Wextra -Werror %s -o %s", next_file, next_exec);
 system(cmdline);
---X;
+if (X > 0)
+{
+sprintf(cmdline, "./%s", next_exec);
+system(cmdline);
+}
 return 0;
 }
