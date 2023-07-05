efa5b2abc4a1dc6535fcc8a660bdb4319977f25c
kvlimon <ammon.pro@gmail.com>
2023-07-05 18:23:08 +0200

    fix: semicolon

--- ASM_X86_64/Colleen.s
+++ ASM_X86_64/Colleen.s
@@ -1,5 +1,5 @@
 section .data
-message: db "section .data%1$cmessage: db %2$c%3$s%2$c%1$c%1$csection .text%1$cglobal main%1$cextern printf%1$c%1$c; Self%1$c;%1$csomething:%1$cret%1$c%1$cmain:%1$c; Propagation%1$cpush rbp%1$cmov rbp, rsp%1$ccall something%1$cmov rcx, message%1$cmov rdx, 34%1$cmov rsi, 10%1$cmov rdi, message%1$ccall printf%1$cmov rsp, rbp%1$cpop rbp%1$cret%1$c"
+message: db "section .data%1$cmessage: db %2$c%3$s%2$c%1$c%1$csection .text%1$cglobal main%1$cextern printf%1$c%1$c; Self%1$c%1$csomething:%1$cret%1$c%1$cmain:%1$c; Propagation%1$cpush rbp%1$cmov rbp, rsp%1$ccall something%1$cmov rcx, message%1$cmov rdx, 34%1$cmov rsi, 10%1$cmov rdi, message%1$ccall printf%1$cmov rsp, rbp%1$cpop rbp%1$cret%1$c"
 
 section .text
 global main
